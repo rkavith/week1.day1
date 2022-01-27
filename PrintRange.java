9f2100f2e880f299e75b3ec70ea0a81cffd51146
rkavith <98160970+rkavith@users.noreply.github.com>
2022-01-27 22:35:46 +0530

    Add files via upload

---
+++ PrintRange.java
@@ -0,0 +1,16 @@
+package Selenium.week1.day2;
+
+public class PrintRange {
+	public static void main(String[] args) {
+
+		int range= 5;
+		int sum=0;
+
+		for (int j=1; j<=range; j++)
+		{
+			sum=sum+j;
+			System.out.println(sum);
+		}
+
+	}
+}
